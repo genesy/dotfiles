62310d0f6ca5f1f0b96252e06fb405efab6daa89
Gene Sy <gnsyftw@gmail.com>
2020-02-18 05:58:43 +0800

    more karabiner changes

--- macos/karabinersetup.sh
+++ macos/karabinersetup.sh
@@ -1,1 +1,3 @@
-ln ~/dotfiles/macos/karabinerprofile.json ~/.config/karabiner/assets/gene.json
+ln ~/dotfiles/macos/karabinerprofile.json ~/.config/karabiner/assets/complex_modifications/gene.json
+
+# karabiner://karabiner/assets/complex_modifications/import?url=file:///Users/genesy/.config/karabiner/assets/complex_modifications/gene.json
