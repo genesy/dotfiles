2045410e7eea90a0a7f48dd1bc17144eb4787cff
Gene Sy <gnsyftw@gmail.com>
2025-08-13 16:16:49 +0800

    updates aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,37 +1,23 @@
 alias reload='source ~/.zshrc'
 
 alias vi="nvim"
-alias svi="sudo -E nvim"
-# alias ls="ls -a"
 alias cl="clear"
-alias vg="vagrant"
-alias vgsh="vagrant ssh"
-alias vgup="vagrant up"
+alias dc="docker compose"
+alias g="git"
+alias ga="git add"
+alias gc="git commit"
 alias gcn="git commit --no-verify"
-alias gps="git push"
-alias gpl="git pull"
 alias gignore="git update-index --assume-unchanged"
 alias glh="git log -1 --pretty-format:%h"
+alias gps="git push"
+alias gpl="git pull"
+alias gs="g status"
 alias y="yarn"
 alias yi="yarn install"
-alias yl="yarn link"
-alias yul="yarn unlink"
-alias g="git"
-alias gs="g status"
 alias co="git checkout"
 alias nr="npm run"
-alias tmux="tmux -2"
-alias tm="tmux"
-alias to="tmux detach -a"
-alias td="tmux detach"
-alias tda="tmux detach -a"
-alias ta="tmux attach"
-alias tks="tmux kill-server"
 alias zj="zellij"
 alias xx="exit"
-# for macos
-alias chrome='open -n -a "Google Chrome" --args "--user-data-dir=$HOME/XChrome"'
-alias ct="ctags -r --exclude=node_modules --exclude=bower_components --exclude=dist -f ./.ctags ."
 
 # filesystem aliases
 alias cd..='cd ..'
@@ -40,26 +26,14 @@ alias ...='cd ../..'
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
 
-alias sd="cd ~/stelladot"
-
 # ip addresses
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en1"
 alias ips="ifconfig -a | perl -nle'/(\d+\.\d+\.\d+\.\d+)/ && print $1'"
 
 
-# flush directory service cache
-# alias flush="dscacheutil -flushcache"
-
-
-# recursively delete `.ds_store` files
-alias cleanup="find . -name '*.ds_store' -type f -ls -delete"
-
 alias mkdir="mkdir -p"
 alias rmrf="rm -rf"
-
-# empty the trash on all mounted volumes and the main hdd
-# alias emptytrash="sudo rm -rfv /volumes/*/.trashes; rm -rfv ~/.trash"
 
 alias p="ping -i 0.2"
 # alias p0="ping -i 0.2 `netstat -nr | grep -m 1 '^default' | awk '{print $2;}'`"
@@ -67,26 +41,8 @@ alias pg="p 8.8.8.8"
 alias p1="p 192.168.1.1"
 alias p2="p 192.168.1.2"
 alias p3="p 192.168.1.3"
-# alias pg="postgres -d /usr/local/var/postgres"
-
 
 alias ss="cl && source ~/.zshrc"
 
-alias if0="sudo ifconfig en0"
-alias wpw="webpack --progress --colors --watch"
-
-alias removetag='git push origin :refs/tags/$1'
-alias gpt= 'git push && git push --tags'
-
-alias clip='clip.exe'
-
-alias sd='dev $1';
 alias gpss='git push -u origin HEAD'
 
-function squash() {
-  git reset $(git merge-base ${1:master} $(git branch --show-current))
-}
-
-# setsid program-name &>/dev/null
-# alias code='setsid /mnt/c/Program\ Files/Microsoft\ VS\ Code/Code.exe /c "code" &>/dev/null'
-# alias code='/mnt/c/Program\ Files/Microsoft\ VS\ Code/Code.exe /c "code"'
