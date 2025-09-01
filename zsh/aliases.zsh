e37963ee595fb7bcb86f39b342a515dfbcd6fad4
Gene Sy <gnsyftw@gmail.com>
2025-09-01 11:40:23 +0800

    yes

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -23,7 +23,7 @@ alias xx="exit"
 
 if command -v eza >/dev/null 2>&1; then
   # alias ls='eza --long --git --smart-group'
-  alias ls='eza --hyperlink --icons --all'
+  alias ls='eza --hyperlink --icons --all -w=80'
   alias lst='ls --tree'
 else
   alias ls='ls --color=auto'  # or just 'ls'
@@ -57,3 +57,20 @@ alias ss="cl && source ~/.zshrc"
 
 alias gpss='git push -u origin HEAD'
 
+cdw() {
+  local winpath="$1"
+
+  # replace backslashes with forward slashes
+  winpath="${winpath//\\//}"
+
+  # extract drive letter (first char, lowercased)
+  local drive=$(printf "%s" "$winpath" | head -c1 | awk '{print tolower($0)}')
+
+  # remove drive letter + colon
+  local subpath="${winpath:2}"
+
+  # build WSL path
+  local wsl_path="/mnt/$drive$subpath"
+
+  cd "$wsl_path" || echo "Path not found: $wsl_path"
+}
